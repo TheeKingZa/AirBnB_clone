bd8a511318599e6ad7730c3a1ab1efbdd6aaca7f
Pule <proceedingmc@gmail.com>
2023-10-14 23:13:03 +0200

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -6,18 +6,20 @@ from models import storage
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
-        """Initialize a new BaseModel
+        """Initialize a new BaseModel instance
+        with id, created_at, and updated_at attributes.
 
-        instance with id, created_at, and updated_at attributes.
+        If 'kwargs' is not empty, it populates the instance attributes
+            from the dictionary representation.
 
-        If kwargs is not empty, it populates the instance
-            attributes from the dictionary representation.
+        :param kwargs:
+            Dictionary containing attribute values.
         """
         if kwargs:
             for key, value in kwargs.items():
                 if key != '__class__':
                     setattr(self, key, value)
-            self.update_at = datetime.now()
+            self.updated_at = datetime.now()
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
@@ -44,7 +46,7 @@ class BaseModel:
         Return a string representation of the BaseModel instance.
         """
         return "[{}] ({}) {}".format(
-                self.__class__.__name__,
+                self.__class__.__name,
                 self.id,
                 self.__dict__
         )
