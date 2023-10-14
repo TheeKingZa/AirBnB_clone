43252fecb2934ca256b3ae6dc7ff25e0c6e93785
Pule <proceedingmc@gmail.com>
2023-10-14 22:04:36 +0200

    console try

--- console.py
+++ console.py
@@ -1,34 +1,26 @@
 #!/usr/bin/env python3
-# console.py
+"""
+This module defines a command-line interpreter for the AirBnB project.
+"""
 
 import cmd
 
+
 class HBNBCommand(cmd.Cmd):
-    """
-    Command interpreter for the AirBnB clone.
-    """
     prompt = "(hbnb) "
 
-    def do_quit(self, line):
-        """
-        Quit command to exit the program
-        """
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
         return True
 
-    def help_quit(self):
-        print("Quit command to exit the program")
-
-    def do_EOF(self, line):
-        """
-        Quit the command interpreter.
-        """
+    def do_EOF(self, arg):
+        """Exit the program using Ctrl+D (EOF)"""
         return True
 
     def emptyline(self):
-        """
-        Do nothing when an empty line is entered.
-        """
+        """Do nothing when an empty line is entered"""
         pass
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
