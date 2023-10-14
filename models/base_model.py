4bc3c434ca2952a8481d64716ec6c2f7403629cb
Pule <proceedingmc@gmail.com>
2023-10-14 22:27:18 +0200

    all files added

--- models/base_model.py
+++ models/base_model.py
@@ -7,7 +7,7 @@ from models import storage
 class BaseModel:
     def __init__(self, *args, **kwargs):
         """Initialize a new BaseModel
-    
+
         instance with id, created_at, and updated_at attributes.
 
         If kwargs is not empty, it populates the instance
@@ -16,7 +16,7 @@ class BaseModel:
         if kwargs:
             for key, value in kwargs.items():
                 if key != '__class__':
-                        setattr(self, key, value)
+                    setattr(self, key, value)
             self.update_at = datetime.now()
         else:
             self.id = str(uuid.uuid4())
@@ -43,4 +43,8 @@ class BaseModel:
         """
         Return a string representation of the BaseModel instance.
         """
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(
+                self.__class__.__name__,
+                self.id,
+                self.__dict__
+        )
